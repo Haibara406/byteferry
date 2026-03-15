4c289980e6ea74f4c190ce8024db24385d0eca94
Haibaraiii <haibara406@gmail.com>
2026-03-15 12:23:23 +0800

    first commit

---
+++ src/main/java/com/byteferry/byteferry/ByteFerryApplication.java
@@ -0,0 +1,13 @@
+package com.byteferry.byteferry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteFerryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteFerryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/byteferry/byteferry/ByteFerryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byteferry.byteferry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteFerryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
